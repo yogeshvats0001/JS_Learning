31fa60ddbcf5e6e87cd413eba03ceb128b8f4c54
yogesh <yogisharma1062@gmail.com>
2023-08-29 04:38:09 +0530

    Block Scope

---
+++ js-concepts/blockScope.js
@@ -0,0 +1,14 @@
+// Block Scope in JS::
+{
+    var a = 10
+    let b = 20
+    const c = 30
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
---
+++ js-concepts/shadowing.js
@@ -0,0 +1,6 @@
+var a = 100
+{
+    var a = 200
+    console.log(a)
+}
+console.log(a)
